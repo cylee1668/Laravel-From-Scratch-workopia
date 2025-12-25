82962e19d0e1c355e286bfea1d27349ec5d989a5
cylee1668 <cylee1668@gmail.com>
2025-12-25 10:25:14 +0800

    section 19 end

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header(),
+Route::get('/jobs', function(){
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
 
-    ];
+Route::get('/header', function(){
+    return response('<h1>Hello World</h1>', 200)->header('Content-Type', 'text/plain');
+});
+
+Route::get('/json', function(){
+    return response()->json(['name' => 'John Doe'])->cookie('name', 'Brad');
+});
+
+Route::get('/download', function(){
+    return response()->download(public_path('favicon.ico'));
+});
+
+Route::get('/read-cookie', function(Request $request){
+    $cookieValue = $request->cookie('name');
+    return response(['cookie' => $cookieValue]);
+});
+
+Route::get('/test2', function(){
+    return 'TEST@2';
 });
