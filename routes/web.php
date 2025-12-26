312927eecde1f58b8ccbecc4f6a7bfcea7f23934
cylee1668 <cylee1668@gmail.com>
2025-12-26 15:19:27 +0800

    section 26 end leared more about controller

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/jobs/create', [JobController::class, 'create']);
+
+Route::get('/', [HomeController::class, 'index']);
