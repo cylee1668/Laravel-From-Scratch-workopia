c9586c060671238621fbe03054ef055c6f340b02
cylee1668 <cylee1668@gmail.com>
2025-12-27 17:48:07 +0800

    section 35 end notable to use npx tailwind, but use CDN

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>{{$title ?? 'Workopia | Finad and list Jobs'}}</title>
 </head>
 
