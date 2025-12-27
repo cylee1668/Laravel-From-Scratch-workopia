0637df1ce24407d53daca81897fdd9b006dfdcb5
cylee1668 <cylee1668@gmail.com>
2025-12-27 12:31:00 +0800

    section 31 end Learned layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,8 @@
 </head>
 
 <body class="bg-gray-100">
-    <h1>Welcome to Workopia</h1>
+
+    @include('partials.navbar')
     <main class="container mx-auto p-4 mt-4">
         @yield('content')
     </main>
