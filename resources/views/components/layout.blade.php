f5acc6d3d826a1ab0ec7b04e8c6244e0f9eb804e
cylee1668 <cylee1668@gmail.com>
2025-12-28 19:53:10 +0800

    section 42 end, hero component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,15 +9,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>{{ $title ?? 'Workopia | Finad and list Jobs' }}</title>
 </head>
 
 <body class="bg-gray-100">
 
     <x-header />
+    @if(request()->is('/'))
+    <x-hero title="Hello" />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
+
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
