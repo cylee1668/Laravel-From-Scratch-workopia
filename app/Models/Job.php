1756f57713ee3e23f1a00523184c8012c268ead6
cylee1668 <cylee1668@gmail.com>
2025-12-31 19:10:59 +0800

    section 59 end, JobSchema Update

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,5 +11,23 @@ class Job extends Model
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'description'];
+    protected $fillable = [
+        'title',
+        'description',
+        'salary',
+        'tags',
+        'job_type',
+        'remote',
+        'requirements',
+        'benefits',
+        'address',
+        'city',
+        'zipcode',
+        'contact_email',
+        'contact_phone',
+        'company_name',
+        'company_description',
+        'company_logo',
+        'company_website'
+    ];
 }
