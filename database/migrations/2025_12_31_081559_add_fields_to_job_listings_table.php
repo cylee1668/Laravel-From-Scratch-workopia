83acc2a77236a0860f707fb7e4d03be5abd6cfe0
cylee1668 <cylee1668@gmail.com>
2026-01-05 11:52:44 +0800

    section 60 End, learn how to create two table relate relation in migraiton file

--- database/migrations/2025_12_31_081559_add_fields_to_job_listings_table.php
+++ database/migrations/2025_12_31_081559_add_fields_to_job_listings_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Container\Attributes\DB;
+//use Illuminate\Container\Attributes\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,14 +16,18 @@ return new class extends Migration
         DB::table('job_listings')->truncate();
 
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('id');
+
+
             $table->integer('salary');
             $table->string('tags')->nullable();
-            $table->enum('job_type',['Full-Time', 'Part-Time','Contract','Temporary','Intership','Volunteer','on-Call'])->default('Full-Time');
+            $table->enum('job_type', ['Full-Time', 'Part-Time', 'Contract', 'Temporary', 'Intership', 'Volunteer', 'on-Call'])->default('Full-Time');
             $table->boolean('remote')->default(false);
             $table->string('requirements')->nullable();
             $table->string('benefits')->nullable();
             $table->string('address')->nullable();
-            $table->string('city')->nullable();
+            $table->string('city');
+            $table->string('state');
             $table->string('zipcode')->nullable();
             $table->string('contact_email');
             $table->string('contact_phone')->nullable();
@@ -31,6 +35,9 @@ return new class extends Migration
             $table->string('company_description')->nullable();
             $table->string('company_logo')->nullable();
             $table->string('company_website')->nullable();
+
+            //Add user foriegn key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -40,7 +47,27 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
-            $table->dropColumn(['salary','tags','job_type','remote','requirements','benefits','address','city','zipcode','contact_email','contact_phone','company_name','company_description','company_logo','company_website'])
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+
+
+            $table->dropColumn([
+                'salary',
+                'tags',
+                'job_type',
+                'remote',
+                'requirements',
+                'benefits',
+                'address',
+                'city',
+                'zipcode',
+                'contact_email',
+                'contact_phone',
+                'company_name',
+                'company_description',
+                'company_logo',
+                'company_website'
+            ]);
         });
     }
 };
