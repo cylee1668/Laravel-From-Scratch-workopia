e760d696fe291cffbfc8e33cace429e785bf980b
cylee1668 <cylee1668@gmail.com>
2026-01-09 15:09:11 +0800

    section72 end,create components of text-area,file,select

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,72 +10,43 @@
                 Job Info
             </h2>
 
-            <x-inputs.text id="title" name="title" label="Job Title" placeholder="Software Engineer"/>
+            <x-inputs.text id="title" name="title" label="Job Title" placeholder="Software Engineer" />
+
+            <x-inputs.text-area id="description" name="description" label="Job Description"
+                placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..." />
 
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="description">Job Description</label>
-                <textarea cols="30" rows="7" id="description" name="description"
-                    class="w-full px-4 py-2 border rounded focus:outline-none @error('description') border-red-500 @enderror"
-                    placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team...">{{ old('description') }}</textarea>
-                @error('description')
-                    <p class="text-red-500 text-sm mt-1">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
+            <x-inputs.text id="salary" name="salary" label="Annual Salary" type="number" placeholder="90000" />
 
-            <x-inputs.text id="salary" name="salary" label="Annual Salary" type="number" placeholder="90000"/>
+            <x-inputs.text-area id="requirements" name="requirements" label="Requirements"
+                placeholder="Bachelor's degree in Computer Science" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="requirements">Requirements</label>
-                <textarea id="requirements" name="requirements" class="w-full px-4 py-2 border rounded focus:outline-none"
-                    placeholder="Bachelor's degree in Computer Science"></textarea>
-            </div>
+            <x-inputs.text-area id="benefit" name="benefit" label="Benefits"
+                placeholder="Health insurance, 401k, paid time off" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="benefits">Benefits</label>
-                <textarea id="benefits" name="benefits" class="w-full px-4 py-2 border rounded focus:outline-none"
-                    placeholder="Health insurance, 401k, paid time off"></textarea>
-            </div>
+            <x-inputs.text id="tags" name="tags" label="tags (comma-separated)"
+                placeholder="development, coding, java, python" />
 
-            <x-inputs.text id="tags" name="tags" label="tags (comma-separated)" placeholder="development, coding, java, python"/>
+            <x-inputs.select id="job_type" name="job_type" label="Job Type" value="{{ old('job_type') }}"
+                :options="[
+                    'Full-time' => 'Full-time',
+                    'Part-Time' => 'Part-Time',
+                    'Contract' => 'Contract',
+                    'Temporary' => 'Temporary',
+                    'Internship' => 'Internship',
+                    'Volunteer' => 'Volunteer',
+                    'On-Call' => 'On-Call',
+                ]" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="job_type">Job Type</label>
-                <select id="job_type" name="job_type"
-                    class="w-full px-4 py-2 border rounded focus:outline-none @error('job_type') border-red-500 @enderror">
-                    <option value="Full-Time" {{ old('job_type') == 'Full-Time' ? 'selected' : '' }}>
-                        Full-Time
-                    </option>
-                    <option value="Part-Time" {{ old('job_type') == 'Part-time' ? 'selected' : '' }}>Part-Time</option>
-                    <option value="Contract" {{ old('job_type') == 'Contract' ? 'selected' : '' }}>Contract</option>
-                    <option value="Temporary" {{ old('job_type') == 'Temporary' ? 'selected' : '' }}>Temporary</option>
-                    <option value="Internship " {{ old('job_type') == 'Internship' ? 'selected' : '' }}>Internship
-                    </option>
-                    <option value="Volunteer" {{ old('job_type') == 'Volunteer' ? 'selected' : '' }}>Volunteer</option>
-                    <option value="On-Call" {{ old('job_type') == 'On-Call' ? 'selected' : '' }}>On-Call</option>
-                </select>
-                @error('job_type')
-                    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+            <x-inputs.select id="remote" name="remote" label="Remote" :options="[0 => 'No', 1 => 'Yes']" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="remote">Remote</label>
-                <select id="remote" name="remote" class="w-full px-4 py-2 border rounded focus:outline-none">
-                    <option value="false">No</option>
-                    <option value="true">Yes</option>
-                </select>
-            </div>
+            <x-inputs.text id="address" name="address" label="Address" placeholder="123 Main St" />
 
-            <x-inputs.text id="address" name="address" label="Address" placeholder="123 Main St"/>
+            <x-inputs.text id="city" name="city" label="City" placeholder="Albany" />
 
-            <x-inputs.text id="city" name="city" label="City" placeholder="Albany"/>
+            <x-inputs.text id="state" name="state" label="State" placeholder="NY" />
 
-            <x-inputs.text id="state" name="state" label="State" placeholder="NY"/>
-
-            <x-inputs.text id="zipcode" name="zipcode" label="zipcode" placeholder="02214"/>
+            <x-inputs.text id="zipcode" name="zipcode" label="zipcode" placeholder="02214" />
 
 
 
@@ -83,19 +54,22 @@
                 Company Info
             </h2>
 
-           <x-inputs.text id="company_name" name="company_name" label="Company name" placeholder="Enter Company Name"/>
+            <x-inputs.text id="company_name" name="company_name" label="Company name"
+                placeholder="Enter Company Name" />
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="company_description">Company Description</label>
-                <textarea id="company_description" name="company_description"
-                    class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Company Description"></textarea>
-            </div>
+            <x-inputs.text-area id="company_description" name="company_description" label="Company Description"
+                placeholder="Enter Company Description" />
 
-            <x-inputs.text id="company_website" name="company_website" label="Company Website" placeholder="Enter Company website"/>
 
-            <x-inputs.text id="contact_phone" name="contact_phone" label="Contact Phone" placeholder="Enter Contact Phone"/>
 
-            <x-inputs.text id="contact_email" name="contact_email" label="Contact email" placeholder="Enter Contact Email" type="email"/>
+            <x-inputs.text id="company_website" name="company_website" label="Company Website"
+                placeholder="Enter Company website" />
+
+            <x-inputs.text id="contact_phone" name="contact_phone" label="Contact Phone"
+                placeholder="Enter Contact Phone" />
+
+            <x-inputs.text id="contact_email" name="contact_email" label="Contact email"
+                placeholder="Enter Contact Email" type="email" />
             <div class="mb-4">
                 <label class="block text-gray-700" for="contact_phone">Contact Phone</label>
                 <input id="contact_phone" type="text" name="contact_phone"
@@ -109,14 +83,7 @@
                     placeholder="Email where you want to receive applications" />
             </div>
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="company_logo">Company Logo</label>
-                <input id="company_logo" type="file" name="company_logo"
-                    class="w-full px-4 py-2 border rounded focus:outline-none  @error('company_logo') border-red-500 @enderror" />
-                @error('company_logo')
-                    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+            <x-inputs.file id="company_logo" name="company_logo" label="Company Logo"/>
 
             <button type="submit"
                 class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
