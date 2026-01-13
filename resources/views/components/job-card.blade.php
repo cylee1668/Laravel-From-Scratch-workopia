06e1a6abec53d3ecced54d620009dc9213c3acda
cylee1668 <cylee1668@gmail.com>
2026-01-13 10:41:01 +0800

    section 76 end, using if and if to control optional fields

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -2,7 +2,9 @@
 
 <div class="rounded-lg shadow-md bg-white p-4">
     <div class="flex items-center space-between gap-4">
-        <img src="/images/{{ $job->company_logo }}" alt="{{ $job->company_name }}" class="w-14" />
+        @if ($job->company_logo)
+            <img src="/images/{{ $job->company_logo }}" alt="{{ $job->company_name }}" class="w-14" />
+        @endif
         <div>
             <h2 class="text-xl font-semibold">
                 {{ $job->title }}
@@ -24,9 +26,11 @@
             @endif
 
         </li>
+        @if($job->tags)
         <li class="mb-2">
-            <strong>Tags: {{ucwords(str_replace(',', ', ', $job->tags))}}</strong>
+            <strong>Tags: {{ ucwords(str_replace(',', ', ', $job->tags)) }}</strong>
         </li>
+        @endif
     </ul>
     <a href="{{ route('jobs.show', $job->id) }}"
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
