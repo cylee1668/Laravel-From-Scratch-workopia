b50d58f0e3423311750cd6a4aa28a2ebbae2ac81
cylee1668 <cylee1668@gmail.com>
2026-01-21 14:20:02 +0800

    section 100 end, upload avatar

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
     // @desc Update profile info
     // @route PUT /profile
-    public function update(Request $request): RedirectResponse {
+    public function update(Request $request): RedirectResponse
+    {
         //Get logger in user
         $user = Auth::user();
 
@@ -18,10 +20,27 @@ class ProfileController extends Controller
         $validatedData = $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|email',
+            'avatar' => 'nullable|image|mimes:jpeg,jpg,png,gif|max:2048',
         ]);
 
+        //Get user name and email
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        //Handle avatar upload
+        if ($request->hasFile('avatar')) {
+            //Delete old avator if exists
+            if ($user->avatar) {
+                Storage::delete('public/' . $user->avatar);
+            }
+
+            // Store new avatar
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+            $user->avatar = $avatarPath;
+        }
+
         //updaate user info
-        $user->update($validatedData);
+        $user->save();
 
         return redirect()->route('dashboard')->with('success', 'Profile info updated!');
     }
