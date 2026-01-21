7eb6e1e36dcee602871ccf8935724d52234d1a93
cylee1668 <cylee1668@gmail.com>
2026-01-21 11:15:01 +0800

    section 99 end, User profile name and email update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,45 +1,63 @@
 <x-layout>
-    <div class="bg-white p-8 rounded-lg shadow-md w-full">
-        <h3 class="text-3xl text-center font-bold mb-4">
-            My Job Listings
-        </h3>
-        @forelse ($jobs as $job)
-            {{-- <pre>
-                    {{ var_dump($job->toArray()) }}
-            </pre> --}}
-            {{-- @dump($job->getOriginal()) --}}
+    <section class="flex flex-col md:flex-row gap-4">
+        {{-- Profile Info Form --}}
+        <bg-white class="p-8 rounded-lg shadow-md w-full">
+            <h3 class="text-3xl text-center font-bold mb-4">
+                Profile Info
+            </h3>
 
-            <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
+            <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <x-inputs.text id="name" name="name" label="Name" value="{{ $user->name }}" />
+                <x-inputs.text id="email" name="email" label="Email" type="email"
+                    value="{{ $user->email }}" />
 
 
-                <div>
-                    <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
-                    <p class="text-gray-700">{{ $job->job_type }}</p>
+                <button type="submit"
+                    class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 border rounded focus:outline-none">
+                    Save
+                </button>
+
+            </form>
+        </bg-white>
+
+        {{-- Job Listing --}}
+        <div class="bg-white p-8 rounded-lg shadow-md w-full">
+            <h3 class="text-3xl text-center font-bold mb-4">
+                My Job Listings
+            </h3>
+            @forelse ($jobs as $job)
+                <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
+
+
+                    <div>
+                        <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
+                        <p class="text-gray-700">{{ $job->job_type }}</p>
+                    </div>
+                    <div class="flex space-x-3">
+                        <a href="{{ route('jobs.edit', $job->id) }}"
+                            class="bg-blue-500 text-white px-4 py-2 rounded text-sm">Edit</a>
+                        <!-- Delete Form -->
+                        <form method="POST" action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
+                            onsubmit="return confirm('Are you sure thart you want to delete this job?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="px-4 py-2 text-sm bg-red-500 hover:bg-red-600 text-white rounded">
+                                Delete
+                            </button>
+                        </form>
+                        <!-- End Delete Form -->
+                    </div>
+
                 </div>
-                <div class="flex space-x-3">
-                    <a href="{{ route('jobs.edit', $job->id) }}"
-                        class="bg-blue-500 text-white px-4 py-2 rounded text-sm">Edit</a>
-                    <!-- Delete Form -->
-                    <form method="POST" action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
-                        onsubmit="return confirm('Are you sure thart you want to delete this job?')">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">
-                            Delete
-                        </button>
-                    </form>
-                    <!-- End Delete Form -->
-                </div>
-
-
-
-
-
-            </div>
-        @empty
-            <p class="text-gray-700">You have no job listings</p>
-        @endforelse
-        <p>{{ $user->name }}</p>
-    </div>
+            @empty
+                <p class="text-gray-700">You have no job listings</p>
+            @endforelse
+        </div>
+    </section>
+    <x-bottom-banner />
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\LogRequest;
 
 
@@ -38,3 +39,5 @@ Route::middleware('guest')->group(function () {
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::put('/profile',[ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
