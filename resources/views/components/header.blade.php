854ea0425ba3af77f0beaad2cbaeb57ab951d044
cylee1668 <cylee1668@gmail.com>
2026-01-21 16:19:37 +0800

    section 101 end add avatar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,8 +10,22 @@
             @auth
                 <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
 
-                <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-                <x-button-link url='/jobs/create' icon='edit'>Create Job</x-button-link>
+                {{-- <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link> --}}
+
+                <div class="flex items-center space-x-3">
+                    <a href="{{ route('dashboard') }}">
+                        @if (Auth::user()->avatar)
+                            <img src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="{{ Auth::user()->name }}"
+                                class="w-10 h-10 rounded-full" />
+                        @else
+                            <img src="{{ asset('storage/avatars/default-avatar.png') }}" alt="avatar"
+                                class="w-10 h-10 rounded-full" />
+                        @endif
+                    </a>
+                </div>
+                <x-button-link url='/jobs/create' icon='edit'>
+                    Create Job
+                </x-button-link>
                 <x-logout-button />
             @else
                 <x-nav-link url="/login" :active="request()->is('login')" icon="user">Login</x-nav-link>
