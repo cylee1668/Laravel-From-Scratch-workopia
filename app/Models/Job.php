6ddd91a42e971cf6aa8e8f3d6fc2169de24f10e3
cylee1668 <cylee1668@gmail.com>
2026-01-22 16:40:38 +0800

    section 105 Bookmark migrate

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
@@ -39,5 +40,10 @@ class Job extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    // Relation to bookmarks
+    Public function bookmarkByUsers(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
